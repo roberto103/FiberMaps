8e235e76024bbde4e7016c512706252115ae6898
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-12-28 20:04:10 -0300

    Adicionando marcador com o click

--- src/js/maps.js
+++ src/js/maps.js
@@ -2,18 +2,23 @@
 function initMap() {
     const location = { lat: -7.8502794, lng: -35.7670493 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: location,
-      mapTypeId: 'satellite',
-      mapTypeControl: false
+        zoom: 16,
+        center: location,
+        mapTypeId: 'satellite',
+        mapTypeControl: false
     });
 
-    map.addListener('click', function(e){
-        console.log(e);
-    });
+    // Adiciona novo marcador
+    map.addListener('click', function (e) {
 
-    const marker = new google.maps.Marker({
-      position: location,
-      map: map,
+        var ClickPosition = e.latLng;
+
+        new google.maps.Marker({
+            position: ClickPosition,
+            map: map,
+            title: 'CTO',
+            alt: 'C'
+        });
+
     });
 }
